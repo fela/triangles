ce8046004b2d70990cc94da662e6ac827582455f
Fela Winkelmolen <felawinkelmolen@gmail.com>
2014-10-28 15:38:35 +0100

    update gitignore and add test to convert lists to sets

--- test_triangles.py
+++ test_triangles.py
@@ -1,7 +1,19 @@
 import random
 import unittest
 
-from triangles import subsets
+from triangles import subsets, list_of_set_to_set_of_set
+
+
+class TestListOfSetToSetOfSet(unittest.TestCase):
+
+    def setUp(self):
+        self.seq = range(10)
+
+    def test_two(self):
+        self.assertEqual(list_of_set_to_set_of_set([{1}, {}]),
+                         {frozenset({}), frozenset({1})})
+        #    self.assertEual(subsets({1, 2, 3, 4, 5}), {{}, {1}})
+        
 
 class TestSubset(unittest.TestCase):
 
@@ -9,14 +21,15 @@ class TestSubset(unittest.TestCase):
         self.seq = range(10)
 
     def test_empty(self):
-        self.assertEqual(subsets({}), {{}})
+        self.assertEqual(subsets({}), {frozenset({})})
 
     def test_one(self):
-        self.assertEqual(subsets({1}), {{1}, {}})
+        self.assertEqual(subsets({1}), {frozenset({1}), frozenset({})})
 
     def test_two(self):
-        self.assertEqual(subsets({1, 2}), {{}, {1}, {2}, {1, 2}})
-        
+        self.assertEqual(subsets({1, 2}), {frozenset({}), frozenset({1}),
+                                           frozenset({2}), frozenset({1, 2})})
+
     #def test_five(self):
     #    expected = {
     #        {},
--- triangles.py
+++ triangles.py
@@ -1,2 +1,5 @@
+def list_of_set_to_set_of_set(list):
+    pass
+
 def subsets(set):
     pass
